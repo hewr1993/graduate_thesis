18f5fc17f0e3f84ec4401b43e29dc85d1f98049e
hewr1993 <hewr2010@gmail.com>
2015-12-20 02:57:07 +0800

    new benchmarks

--- benchmark/VisualTrakerBenchmark/view.py
+++ benchmark/VisualTrakerBenchmark/view.py
@@ -13,25 +13,29 @@ import itertools
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--dir", required=True,
-                    help="images directory")
-parser.add_argument("--ground-truth", required=True,
-                    help="ground-truth file")
+parser.add_argument(dest="dir", help="data directory")
 parser.add_argument("--delay", default=40, type=int,
                     help="display delay for cv2.waitKey")
 args = parser.parse_args()
 
+
+def process_line(line):
+    if line.find(",") >= 0:
+        return map(int, line.split(","))
+    else:
+        return map(int, line.split("\t"))
+
 if __name__ == "__main__":
     color = (255, 0, 0)
     thickness = 3
-    img_paths = sorted(glob.glob(os.path.join(args.dir, "*.jpg")))
+    img_paths = sorted(glob.glob(os.path.join(args.dir, "img/*.jpg")))
     cnt_imgs = len(img_paths)
     st_time = time.time()
     for idx, (line, img_path) in enumerate(itertools.izip(
-            open(args.ground_truth),
+            open(os.path.join(args.dir, "groundtruth_rect.txt")),
             img_paths,
     )):
-        x, y, w, h = map(int, line.strip().split(","))
+        x, y, w, h = process_line(line.strip())
         img = cv2.imread(img_path)
         cv2.line(img, (x, y), (x + w, y), color, thickness)
         cv2.line(img, (x + w, y), (x + w, y + w), color, thickness)
