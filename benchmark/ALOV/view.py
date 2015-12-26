6eb600ad7d3ed1f0d15befadb22551dbf97b4c1d
hewr1993 <hewr2010@gmail.com>
2015-12-26 16:14:06 +0800

    enhanced view script

--- benchmark/ALOV/view.py
+++ benchmark/ALOV/view.py
@@ -5,20 +5,51 @@
 import os
 import sys
 import cv2
+import time
 import glob
-
 from alov_reader import load_annotations
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument(dest="annotation_file", type=str)
+parser.add_argument("--width", default=400, type=int)
+parser.add_argument("--height", default=300, type=int)
+args = parser.parse_args()
+# get images directory
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+def get_images_directory(ann_fn):
+    name = os.path.splitext(os.path.basename(ann_fn))[0]
+    i = name.find("_")
+    return os.path.join(
+        BASE_DIR, "imagedata++",
+        name[:i], name,
+    )
+
+args.img_dir = get_images_directory(args.annotation_file)
+
 if __name__ == "__main__":
-    ann_fn = "./alov300++_rectangleAnnotation_full/01-Light/01-Light_video00001.ann"
-    anns = load_annotations(ann_fn)
-    img_dir = "./imagedata++/01-Light/01-Light_video00001"
+    anns = load_annotations(args.annotation_file)
+    sum_time = 0
     for f_idx, coords in anns:
-        img = cv2.imread(os.path.join(img_dir, "%08d.jpg" % f_idx))
+        st_time = time.time()
+        base_img = cv2.imread(os.path.join(args.img_dir, "%08d.jpg" % f_idx))
+        img = cv2.resize(base_img, (args.width, args.height))
+        ss = (float(img.shape[1]) / base_img.shape[1],
+              float(img.shape[0]) / base_img.shape[0])  # size scale
+        coords = [(int(x * ss[0]), int(y * ss[1])) for x, y in coords]
         for i in xrange(len(coords)):
             p, q = coords[i], coords[(i + 1) % len(coords)]
             cv2.line(img, p, q, (0, 0, 255), 1)
-        print "\r%d" % f_idx,
+        #cv2.putText(
+            #img, "frame %d" % f_idx,
+            #(0, 0), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
+            #0.5, (128, 0, 128), 1,
+        #)
+        sum_time += time.time() - st_time
+        print "\r%d-%d, %.2ffps" % (
+            f_idx, anns[-1][0],
+            (f_idx - anns[0][0] + 1) / sum_time,
+        ),
         sys.stdout.flush()
         cv2.imshow('img', img)
         if chr(cv2.waitKey(0) & 0xFF) == 'q':
