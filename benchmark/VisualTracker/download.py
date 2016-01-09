35623681e58ad7ff6fc55e19971f2ef0763df023
hewr1993 <hewr2010@gmail.com>
2016-01-10 01:14:51 +0800

    auto data prepare for VisualTracker

--- benchmark/VisualTracker/download.py
+++ benchmark/VisualTracker/download.py
@@ -3,6 +3,14 @@
 # Created Time: Sun Jan 10 01:08:51 2016
 # Purpose: download data from VisualTrackerBenchmark
 # Mail: hewr2010@gmail.com
+import os
 
 if __name__ == "__main__":
-    pass
+    for line in open("./list.txt"):
+        url = line.strip()
+        fn = url.split("/")[-1]
+        name = os.path.splitext(fn)[0]
+        if not os.path.exists(name):
+            os.system("wget %s" % url)
+            os.system("unzip %s" % fn)
+            os.system("rm %s" % fn)
