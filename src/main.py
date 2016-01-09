683a53c3d6d7b70cf8b8a5f5ce85b6658e06d614
hewr1993 <hewr2010@gmail.com>
2016-01-10 02:34:56 +0800

    main.pyeval

--- src/main.py
+++ src/main.py
@@ -11,14 +11,17 @@ from utils import draw_polygon
 if __name__ == "__main__":
     for token, data_stream in get_instances():
         print token
+        results, gt_results = [], []
         for idx, (img, gt_coords) in enumerate(data_stream):
             if idx == 0:
                 tracker = NaiveTracker(img, gt_coords)
             else:
                 coords = tracker.track(img)
-                # TODO draw something
+                results.append(coords)
+                gt_results.append(gt_coords)
                 draw_polygon(img, gt_coords, color=(0, 255, 0))
                 draw_polygon(img, coords, color=(0, 0, 255))
                 cv2.imshow('img', img)
                 if chr(cv2.waitKey(0) & 0xFF) == 'q':
                     exit()
+        # TODO evaluate results
