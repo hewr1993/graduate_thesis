e3925810b3b2da20d23fb9a9ad069617f8f76a50
hewr1993 <hewr2010@gmail.com>
2016-01-26 11:11:49 +0800

    refactor main

--- src/main.py
+++ src/main.py
@@ -11,10 +11,8 @@ from tracker import *
 from utils import draw_polygon, fit_image_in_box
 
 
-def evaluate(tracker_type=NaiveTracker, EXTRA_CANDIDATES=0,
-             display_screen=(600, 800)):
+def show(tracker_type=NaiveTracker, display_screen=(600, 800)):
     """
-    @param EXTRA_CANDIDATES: how many candidates should we draw
     @param display_screen: (height, width). None for no display
     """
     for token, data_stream in get_instances():
@@ -27,7 +25,7 @@ def evaluate(tracker_type=NaiveTracker, EXTRA_CANDIDATES=0,
         for idx, (img, gt_coords) in pbar(enumerate(data_stream)):
             img = fit_image_in_box(img, display_screen)
             if idx == 0:
-                tracker = tracker_type(img, gt_coords)
+                tracker = tracker_type(img, gt_coords, fixed_size=True)
             else:
                 coords = tracker.track(img)
                 results.append(coords)
@@ -36,23 +34,33 @@ def evaluate(tracker_type=NaiveTracker, EXTRA_CANDIDATES=0,
                 # plot particles
                 for particle in sorted(
                         tracker.particles, key=lambda x: x.weight,
-                )[:EXTRA_CANDIDATES]:
+                ):
                     po = particle.center()
                     po = (int(po[0] * board.shape[1]),
                           int(po[1] * board.shape[0]))
                     cv2.circle(board, po, 1, (255, 0, 0), 1)
                 # plot tracking results
+                if False:  # particle nearest to ground truth
+                    particle = min(tracker.particles,
+                                   key=lambda o: (o.coords[0][0] - gt_coords[0][0]) ** 2 + (o.coords[0][1] - gt_coords[0][1]) ** 2
+                                   ).copy()
+                    draw_polygon(img, particle.coords, color=(0, 255, 255))
                 draw_polygon(img, gt_coords, color=(0, 255, 0))
                 draw_polygon(img, coords, color=(0, 0, 255))
                 # show results
                 cv2.imshow('particles', board)
                 cv2.imshow('img', img)
+                if idx == 1:
+                    cv2.moveWindow("img", 0, 0)
+                    cv2.moveWindow("particles", img.shape[1], 0)
                 keyboard = chr(cv2.waitKey(0) & 0xFF)
                 if keyboard == 'q':
                     exit()
                 elif keyboard == 's':
                     break
-        # TODO evaluate results
+
+# TODO evaluate results
 
 if __name__ == "__main__":
-    evaluate(ParticleFilterTracker, EXTRA_CANDIDATES=-1)
+    show(lambda *args, **kwargs:
+         HistogramParticleFilterTracker(*args, nr_particle=1000, **kwargs))
